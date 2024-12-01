f65a4edeb740ec662fdca346e1be9a37d25eca14
dompdv <dompdv@yahoo.fr>
2024-12-01 11:08:35 +0100

    Day1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,9 +1,22 @@
 defmodule AdventOfCode.Day01 do
-  def part1(args) do
+  def parse(args) do
     args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn s ->
+      [a, b] = String.split(s, " ", trim: true)
+      {String.to_integer(a), String.to_integer(b)}
+    end)
+    |> Enum.unzip()
+  end
+
+  def part1(args) do
+    {l1, l2} = parse(args)
+    Enum.zip(Enum.sort(l1), Enum.sort(l2)) |> Enum.map(fn {a, b} -> abs(a - b) end) |> Enum.sum()
   end
 
   def part2(args) do
-    args
+    {l1, l2} = parse(args)
+    freq = Enum.frequencies(l2)
+    for(n <- l1, do: n * Map.get(freq, n, 0)) |> Enum.sum()
   end
 end
