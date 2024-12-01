0d007eb6a1cdbacd39cd365d8f5da42beb19ec09
dompdv <dompdv@yahoo.fr>
2024-12-01 11:15:30 +0100

    D01 cleaned

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,22 +1,24 @@
 defmodule AdventOfCode.Day01 do
+  import Enum
+
   def parse(args) do
     args
     |> String.split("\n", trim: true)
-    |> Enum.map(fn s ->
+    |> map(fn s ->
       [a, b] = String.split(s, " ", trim: true)
       {String.to_integer(a), String.to_integer(b)}
     end)
-    |> Enum.unzip()
+    |> unzip()
   end
 
   def part1(args) do
     {l1, l2} = parse(args)
-    Enum.zip(Enum.sort(l1), Enum.sort(l2)) |> Enum.map(fn {a, b} -> abs(a - b) end) |> Enum.sum()
+    zip(sort(l1), sort(l2)) |> map(fn {a, b} -> abs(a - b) end) |> sum()
   end
 
   def part2(args) do
     {l1, l2} = parse(args)
-    freq = Enum.frequencies(l2)
-    for(n <- l1, do: n * Map.get(freq, n, 0)) |> Enum.sum()
+    freq = frequencies(l2)
+    for(n <- l1, do: n * Map.get(freq, n, 0)) |> sum()
   end
 end
