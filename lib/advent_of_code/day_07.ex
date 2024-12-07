58c04403de8d48fcafd87b90359abf3ce3d614d7
dompdv <dompdv@yahoo.fr>
2024-12-07 21:44:03 +0100

    various style improvements

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -9,20 +9,17 @@ defmodule AdventOfCode.Day07 do
     {String.to_integer(target), numbers}
   end
 
-  def to_operators_list(n, base, pad) do
-    cvt = list_in_base(n, base) |> reverse()
-    List.duplicate(0, pad - length(cvt)) ++ cvt
-  end
-
-  def list_in_base(n, base) when n < base, do: [n]
-  def list_in_base(n, base), do: [rem(n, base) | list_in_base(div(n, base), base)]
+  def represents_in_base(n, base), do: reverse(digits(n, base))
+  def digits(n, base) when n < base, do: [n]
+  def digits(n, base), do: [rem(n, base) | digits(div(n, base), base)]
+  def pad_with_0s(list, pad), do: List.duplicate(0, pad - length(list)) ++ list
 
   def works?({target, numbers}, operator_types) do
     n_operators = length(numbers) - 1
     n_possibilities = operator_types ** n_operators
 
     reduce_while(0..(n_possibilities - 1), false, fn i, _ ->
-      operators = to_operators_list(i, operator_types, n_operators)
+      operators = represents_in_base(i, operator_types) |> pad_with_0s(n_operators)
       compute = evaluate(numbers, operators)
       if compute == target, do: {:halt, target}, else: {:cont, false}
     end)
