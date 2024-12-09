e532af54c93e31211efc8038791443a4e8319ded
dompdv <dompdv@yahoo.fr>
2024-12-09 07:46:13 +0100

    Day09 p1

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,23 +1,20 @@
 defmodule AdventOfCode.Day09 do
   import Enum
 
-  def add(m, key, val) do
-    case Map.fetch(m, key) do
-      {:ok, l} -> Map.put(m, key, [val | l])
-      :error -> Map.put(m, key, [val])
-    end
-  end
-
   def build_disk([], _, {m, _, free_map, file_map}) do
-    disk = for(i <- 0..(m - 1), into: %{}, do: {i, :free}) |> Map.merge(file_map)
+    disk = for(i <- 0..(m - 1), into: %{}, do: {i, :free}) |> Map.merge(Map.new(file_map))
     {reverse(free_map), file_map, disk}
   end
 
   def build_disk([n | r], :file, {block_id, file_id, free_map, file_map}) do
     new_file_map =
-      reduce(block_id..(block_id + n - 1), file_map, fn i, acc -> Map.put(acc, i, file_id) end)
+      reduce(block_id..(block_id + n - 1), file_map, fn i, acc -> [{i, file_id} | acc] end)
 
     build_disk(r, :free, {block_id + n, file_id + 1, free_map, new_file_map})
+  end
+
+  def build_disk([0 | r], :free, {block_id, file_id, free_map, file_map}) do
+    build_disk(r, :file, {block_id, file_id, free_map, file_map})
   end
 
   def build_disk([n | r], :free, {block_id, file_id, free_map, file_map}) do
@@ -30,17 +27,16 @@ defmodule AdventOfCode.Day09 do
   end
 
   def rearrange({[], _file_map, disk}), do: disk
+  def rearrange({_, [], disk}), do: disk
 
-  def rearrange({[f | r], file_map, disk}) do
-    last_file_block = max_keys(file_map)
+  def rearrange({[f | r_free], [fi | r_fi], disk}) do
+    {last_file_block, file_id} = fi
 
     if f > last_file_block do
       disk
     else
-      file_id = Map.get(file_map, last_file_block)
-      new_file_map = file_map |> Map.delete(last_file_block) |> Map.put(f, file_id)
       new_disk = Map.put(disk, f, file_id) |> Map.put(last_file_block, :free)
-      rearrange({r, new_file_map, new_disk})
+      rearrange({r_free, r_fi, new_disk})
     end
   end
 
@@ -52,11 +48,11 @@ defmodule AdventOfCode.Day09 do
   end
 
   def parse(args) do
-    args |> to_charlist() |> map(&(&1 - ?0)) |> build_disk(:file, {0, 0, [], %{}})
+    args |> to_charlist() |> map(&(&1 - ?0))
   end
 
   def part1(args) do
-    args |> parse() |> rearrange() |> checksum()
+    args |> parse() |> build_disk(:file, {0, 0, [], []}) |> rearrange() |> checksum()
   end
 
   def part2(args) do
@@ -64,6 +60,6 @@ defmodule AdventOfCode.Day09 do
   end
 
   def test(_) do
-    "2333133121414131402"
+    "1023"
   end
 end
