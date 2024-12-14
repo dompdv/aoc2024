807b8d5bca0804e05d48869ab4357c6f0d9c3609
dompdv <dompdv@yahoo.fr>
2024-12-14 07:10:28 +0100

    Day14

--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -18,6 +18,10 @@ defmodule AdventOfCode.Day14 do
     end
   end
 
+  def pos_after(robots, t, grid_size) when is_list(robots) do
+    map(robots, fn robot -> pos_after(robot, t, grid_size) end)
+  end
+
   def pos_after({px, py, vx, vy}, t, {ox, oy}) do
     {bound(px + vx * t, ox), bound(py + vy * t, oy)}
   end
@@ -36,34 +40,70 @@ defmodule AdventOfCode.Day14 do
     end)
   end
 
+  def multiply(map) do
+    reduce(map, 1, fn {_, v}, acc -> acc * v end)
+  end
+
   def part1(args) do
     grid_size = {101, 103}
+    args |> parse() |> pos_after(100, grid_size) |> count_by_quadrant(grid_size) |> multiply()
+  end
 
-    args
-    |> parse()
-    |> map(&pos_after(&1, 100, grid_size))
-    |> count_by_quadrant(grid_size)
-    |> reduce(1, fn {_, v}, acc -> acc * v end)
+  def print_grid(robots, grid_size) do
+    {ox, oy} = grid_size
+
+    for y <- 0..(oy - 1), into: "" do
+      for x <- 0..(ox - 1), into: "" do
+        if Enum.any?(robots, fn {rx, ry} -> rx == x and ry == y end),
+          do: "#",
+          else: "."
+      end
+    end
+    |> IO.puts()
+
+    IO.puts("")
+  end
+
+  # Separate the robots into 3 bands: left, middle, right
+  def bands(robots, {ox, _oy}) do
+    n = length(robots)
+    {left_x, right_x} = {div(ox, 4), ox - div(ox, 4)}
+
+    robots
+    |> Enum.reduce(%{1 => 0, 2 => 0, 3 => 0}, fn {x, _y}, acc ->
+      case x do
+        x when x < left_x -> Map.update(acc, 1, 1, &(&1 + 1))
+        x when x > right_x -> Map.update(acc, 3, 1, &(&1 + 1))
+        _ -> Map.update(acc, 2, 1, &(&1 + 1))
+      end
+    end)
+    |> then(fn b -> b[1] + b[3] < div(n, 4) end)
   end
 
   def part2(args) do
-    args
-  end
+    grid_size = {101, 103}
+    start_pos = args |> parse()
 
-  def test(_) do
-    """
-    p=0,4 v=3,-3
-    p=6,3 v=-1,-3
-    p=10,3 v=-1,2
-    p=2,0 v=2,-1
-    p=0,0 v=1,3
-    p=3,0 v=-2,-2
-    p=7,6 v=-1,-3
-    p=3,0 v=-1,-2
-    p=9,3 v=2,3
-    p=7,3 v=-1,2
-    p=2,4 v=2,-3
-    p=9,5 v=-3,-3
-    """
+    # Trial & error, guided by the fact that we hope that most of the robots will be in the middle band
+    reduce(6200..6300, nil, fn t, acc ->
+      robots = start_pos |> pos_after(t, grid_size)
+
+      if bands(robots, grid_size) do
+        print_grid(robots, grid_size)
+
+        t |> IO.inspect(label: "time")
+      else
+        acc
+      end
+    end)
+
+    # then use your eyes to find the correct time
+    reduce(6200..6400, nil, fn t, _acc ->
+      robots = start_pos |> pos_after(t, grid_size)
+      t |> IO.inspect(label: "time")
+
+      print_grid(robots, grid_size)
+      Process.sleep(100)
+    end)
   end
 end
