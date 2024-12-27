97ab9ffe1b2409f1b675eabbb29885008200aaac
dompdv <dompdv@yahoo.fr>
2024-12-27 10:22:18 +0100

    Day24 p1

--- lib/advent_of_code/day_24.ex
+++ lib/advent_of_code/day_24.ex
@@ -10,30 +10,10 @@ defmodule AdventOfCode.Day24 do
   def parse(line, acc) do
     if String.contains?(line, ":") do
       [key, value] = String.split(line, ": ")
-      res = value == "1"
-      f = fn state -> {Map.put(state, key, res), res} end
-      Map.put(acc, key, f)
+      set_value(acc, key, value == "1")
     else
       [_, x, op, y, z] = Regex.scan(~r/(.*) (.*) (.*) -> (.*)/, line) |> hd()
-
-      f_op =
-        case op do
-          "AND" -> fn x, y -> x && y end
-          "OR" -> fn x, y -> x || y end
-          "XOR" -> fn x, y -> x != y end
-        end
-
-      f =
-        fn state ->
-          c_x = Map.get(state, x)
-          {state, x} = if is_function(c_x), do: c_x.(state), else: {state, c_x}
-          c_y = Map.get(state, y)
-          {state, y} = if is_function(c_y), do: c_y.(state), else: {state, c_y}
-          res = f_op.(x, y)
-          {Map.put(state, z, res), res}
-        end
-
-      Map.put(acc, z, f)
+      set_operation(acc, x, op, y, z)
     end
   end
 
@@ -62,91 +42,28 @@ defmodule AdventOfCode.Day24 do
     Map.put(wires, z, f)
   end
 
+  def number(<<_::utf8, rest::binary>>), do: String.to_integer(rest)
+
+  def bool_to_int(wires, registers, register) do
+    reduce(registers[register], 0, fn wire, acc ->
+      acc + if wires[wire], do: 2 ** number(wire), else: 0
+    end)
+  end
+
   def part1(args) do
     initial_wires = args |> parse()
-    zedes = for {k, _} <- initial_wires, String.starts_with?(k, "z"), do: k
+    registers = %{"z" => for({k, _} <- initial_wires, String.starts_with?(k, "z"), do: k)}
 
     final_wires =
-      reduce(zedes, initial_wires, fn wires, acc ->
+      reduce(registers["z"], initial_wires, fn wires, acc ->
         {new_acc, _} = Map.get(acc, wires).(acc)
         new_acc
       end)
 
-    reduce(zedes, 0, fn z, acc ->
-      "z" <> r = z
-      r = String.to_integer(r)
-      acc + if final_wires[z], do: 2 ** r, else: 0
-    end)
+    bool_to_int(final_wires, registers, "z")
   end
 
   def part2(args) do
     args
-  end
-
-  def test1(_) do
-    """
-    x00: 1
-    x01: 1
-    x02: 1
-    y00: 0
-    y01: 1
-    y02: 0
-
-    x00 AND y00 -> z00
-    x01 XOR y01 -> z01
-    x02 OR y02 -> z02
-    """
-  end
-
-  def test2(_) do
-    """
-    x00: 1
-    x01: 0
-    x02: 1
-    x03: 1
-    x04: 0
-    y00: 1
-    y01: 1
-    y02: 1
-    y03: 1
-    y04: 1
-
-    ntg XOR fgs -> mjb
-    y02 OR x01 -> tnw
-    kwq OR kpj -> z05
-    x00 OR x03 -> fst
-    tgd XOR rvg -> z01
-    vdt OR tnw -> bfw
-    bfw AND frj -> z10
-    ffh OR nrd -> bqk
-    y00 AND y03 -> djm
-    y03 OR y00 -> psh
-    bqk OR frj -> z08
-    tnw OR fst -> frj
-    gnj AND tgd -> z11
-    bfw XOR mjb -> z00
-    x03 OR x00 -> vdt
-    gnj AND wpb -> z02
-    x04 AND y00 -> kjc
-    djm OR pbm -> qhw
-    nrd AND vdt -> hwm
-    kjc AND fst -> rvg
-    y04 OR y02 -> fgs
-    y01 AND x02 -> pbm
-    ntg OR kjc -> kwq
-    psh XOR fgs -> tgd
-    qhw XOR tgd -> z09
-    pbm OR djm -> kpj
-    x03 XOR y03 -> ffh
-    x00 XOR y04 -> ntg
-    bfw OR bqk -> z06
-    nrd XOR fgs -> wpb
-    frj XOR qhw -> z04
-    bqk OR frj -> z07
-    y03 OR x01 -> nrd
-    hwm AND bqk -> z03
-    tgd XOR rvg -> z12
-    tnw OR pbm -> gnj
-    """
   end
 end
